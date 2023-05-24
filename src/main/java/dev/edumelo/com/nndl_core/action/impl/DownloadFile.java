6c0ad35e3f31a5a3d4aa73ffaba306f508dbd23d
EduMelo <dev@edumelo.com.br>
2023-05-23 23:48:57 -0300

    added someway to garantee the file download

--- src/main/java/dev/edumelo/com/nndl_core/action/impl/DownloadFile.java
+++ src/main/java/dev/edumelo/com/nndl_core/action/impl/DownloadFile.java
@@ -2,9 +2,12 @@ package dev.edumelo.com.nndl_core.action.impl;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import dev.edumelo.com.nndl_core.action.Action;
 import dev.edumelo.com.nndl_core.action.ActionException;
@@ -19,14 +22,11 @@ import dev.edumelo.com.nndl_core.webdriver.SeleniumSndlWebDriverWaiter;
 public class DownloadFile extends Action {
 	private static final String TAG = "downloadFile";
 	private static final String URL_TAG = "url";
-	private static final String FILENAME_TAG = "fileName";
 	private URL url;
-	private String fileName;
 	
 	public DownloadFile(Map<String, ?> mappedAction, Map<String, StepElement> mappedElements) {
 		super(mappedAction, mappedElements);
 		this.url = getUrl(mappedAction);
-		this.fileName = getFileName(mappedAction);
 	}
 
 	private URL getUrl(Map<String, ?> mappedAction) {
@@ -37,10 +37,6 @@ public class DownloadFile extends Action {
 			throw new RuntimeException(String.format("Cannot create URL from the string: %s ",
 					urlString));
 		}
-	}
-	
-	private String getFileName(Map<String, ?> mappedAction) {
-		return (String) mappedAction.get(FILENAME_TAG);
 	}
 
 	@Override
@@ -55,31 +51,30 @@ public class DownloadFile extends Action {
 
 	@Override
 	public void runPreviousModification(ActionModificator modificiator) {
-		// TODO Auto-generated method stub
-
+		return;
 	}
 
 	@Override
 	public Advice runNested(String sessionId, SeleniumSndlWebDriver webDriver,
 			SeleniumSndlWebDriverWaiter webDriverWait, IterationContent rootElement)
 					throws ActionException {
-		RemoteWebDriver driver = webDriver.getWebDriver();
-		
-		String code = "var a = document.createElement('a');"+"\n"+
-				String.format("a.href = '%s';", url)+"\n"+
-				String.format("a.download = '%s';", fileName)+"\n"+
-				"document.body.appendChild(a);"+"\n"+
-				"a.click();";
-		
-		driver.executeScript(code);
-		setActionPerformed(true);
-		return new ContinueAdvice();
+		return runElment(webDriver, webDriverWait);
 	}
 
 	@Override
 	public Advice runAction(String sessionId, SeleniumSndlWebDriver webDriver,
 			SeleniumSndlWebDriverWaiter webDriverWait) throws ActionException {
+		return runElment(webDriver, webDriverWait);
+	}
+
+	private Advice runElment(SeleniumSndlWebDriver webDriver,
+			SeleniumSndlWebDriverWaiter webDriverWait) {
 		RemoteWebDriver driver = webDriver.getWebDriver();
+		
+		driver.executeScript("window.open()");
+		List<String> tabs = new ArrayList<>(driver.getWindowHandles());
+		driver.switchTo().window(tabs.get(tabs.size()-1));
+		driver.get("chrome://downloads/");
 		
 		String code = "var a = document.createElement('a');"+"\n"+
 				String.format("a.href = '%s';", url)+"\n"+
@@ -88,6 +83,16 @@ public class DownloadFile extends Action {
 				"a.click();";
 		
 		driver.executeScript(code);
+		
+		String waitCode = "var items = document.querySelector('downloads-manager').shadowRoot.getElementById('downloadsList').items;"+"\n"+
+				"if (items.every(e => e.state === 'COMPLETE')) return items.map(e => e.fileUrl || e.file_url);";
+		
+		webDriverWait.getWebDriverWaiter().withTimeout(getTimeoutSeconds())
+		.until(ExpectedConditions.jsReturnsValue(waitCode));
+		
+		driver.close();
+		driver.switchTo().window(tabs.get(0));
+		
 		setActionPerformed(true);
 		return new ContinueAdvice();
 	}
